3ede9a078123ccf9d5c5870dcf8474882406a3b2
Jack Transue <johnktransue@gmail.com>
2022-06-02 17:15:30 -0400

    style fix

--- pages/data.tsx
+++ pages/data.tsx
@@ -10,12 +10,12 @@ import Layout from "../components/wrappers/Layout";
 const Data: NextPage = () => {
   return (
     <Layout title="data">
-      <div className="flex flex-col justify-center font-mono text-white h-screen w-screen art-bar-bg">
-        <div className="flex flex-col justify-center self-center">
+      <div className="flex flex-col justify-center font-mono text-white h-screen w-screen art-bar-bg overflow-hidden">
+        <div className="flex flex-col justify-center self-center pt-20">
           <h1 className="site-title uppercase text-3xl">Bar Confessions</h1>
         </div>
         <DataRead />
-        <div className="flex flex-row justify-cetner self-center">
+        <div className="flex flex-row justify-cetner self-center h-screen w-screen">
           <img className="px-5" src="assets/img/palmtree.svg" alt="palm tree" />
           <img src="assets/img/palmtreev2.svg" alt="palm tree" />
           <img src="assets/img/palmtree.svg" alt="palm tree" />
